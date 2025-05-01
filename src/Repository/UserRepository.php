b4dc4f5f92e625e91098360457809e003d6fca6b
Bastien <a@a.a>
2025-05-01 15:29:40 +0200

    Fix top shiny

--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -45,6 +45,7 @@ class UserRepository extends ServiceEntityRepository implements PasswordUpgrader
     public function topMonthlyShinies(): array
     {
         $firstDay = new \DateTime('first day of this month');
+        $firstDay->setTime(0, 0, 0);
         $lastDay = (clone $firstDay)->modify('+1 month');
 
         return $this->createQueryBuilder('u')
