313b776c5d717e224b470770330246989771b3d1
Bastien <a@a.a>
2025-05-01 15:06:01 +0200

    Ajout système maintenance

--- src/Event/Listener/MaintenanceListener.php
+++ src/Event/Listener/MaintenanceListener.php
@@ -4,21 +4,26 @@ namespace App\Event\Listener;
 
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpKernel\Event\RequestEvent;
+use Twig\Environment;
 
-class MaintenanceListener{
+class MaintenanceListener
+{
 
-    private $maintenance;
+    public function __construct(
+        private $maintenance,
+        private Environment $twig
+    )
+    {
 
-    public function __construct($maintenance){
-        $this->maintenance = $maintenance;
     }
 
 
-    public function onKernelRequest(RequestEvent $event): void{
+    public function onKernelRequest(RequestEvent $event): void
+    {
 
 
         //On vérifie si le fichier .maintenance n'existe pas
-        if(!file_exists($this->maintenance)){
+        if (!file_exists($this->maintenance)) {
             return;
         }
 
@@ -28,14 +33,12 @@ class MaintenanceListener{
 
         $event->setResponse(
             new Response(
-                'Le site est en maintenance',
+                $this->twig->render('maintenance.html.twig'),
                 Response::HTTP_SERVICE_UNAVAILABLE
             )
         );
         //On stoppe le traitement des évènements
 
         $event->stopPropagation();
-
     }
-
 }
